356aca8515fd9402fa110e6d10512f81ea8ceaed
SR Ganesh Ram <sr.ganeshram@hotmail.com>
2025-01-29 17:37:17 +0530

    Deployment code
    Deployment code

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Works\\Github\\knowledge-base\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Works\\Github\\knowledge-base\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Works\\Github\\knowledge-base\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Works\\Github\\knowledge-base\\src\\css\\custom.css"),
+];
